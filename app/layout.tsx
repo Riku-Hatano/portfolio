dc4c61a520c33383f233e0a14c587297afc64030
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2023-12-16 23:44:31 +0900

    layout.tsx: add title

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" style={{scrollBehavior: "smooth"}}>
+      <title>Riku's Portfolio</title>
       <body className={inter.className}>{children}</body>
     </html>
   )
