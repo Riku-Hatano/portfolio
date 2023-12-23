a1356e067a5f6390cc8fc07877261074cfce3b6a
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2023-12-23 17:04:04 +0900

    added globalstyle, modified fixed title and menu in nav bar

---
+++ components/Common.tsx
@@ -0,0 +1,9 @@
+import { styled } from "@mui/material";
+import { Box, Grid } from "@mui/material";
+import theme from "./Theme";
+
+export const Wrapper = styled(Box)(() => ({
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+}))
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,19 +1,20 @@
 "use client"
 
 import React from "react"
-import { Drawer, List, ListItem, Typography, Grid, IconButton, Link } from "@mui/material"
+import { Drawer, List, Typography, Grid, IconButton, Link } from "@mui/material"
 import { useState } from "react";
 import { styled } from "@mui/material";
 import theme from "./Theme";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { Wrapper } from "./Common";
 
 const Nav = () => {
     const [toggle, setToggle] = useState(false);
     const onToggle = () => {
-        console.log(toggle);
         setToggle(!toggle);
     }
+
     const TypographyTitle = styled(Typography)(({ theme }: any) => ({
         color: theme.palette.secondary.main,
         padding: `${theme.spacing(1)} ${theme.spacing(2)}`,
@@ -23,11 +24,7 @@ const Nav = () => {
             color: theme.palette.primary.main,
         }
     }));
-    const ListWrapper = styled(ListItem)(({ theme }: any) => ({
-        padding: "0",
-        width: "100%",
-    }));
-    const LinkWrapper = styled(Link)(({ theme }: any) => ({
+    const LinkWrapper = styled(Link)(({ theme, tag }: any) => ({
         borderBottom: `1px solid ${theme.palette.secondary.main}`,
         width: "100%",
     }));
@@ -45,34 +42,26 @@ const Nav = () => {
                 }}
             >
                 <List>
-                    <ListWrapper>
+                    <Wrapper>
                         <LinkWrapper href="#top">
-                            <TypographyTitle>
-                                top
-                            </TypographyTitle>
+                            <TypographyTitle children="top" />
                         </LinkWrapper>
-                    </ListWrapper>
-                    <ListWrapper>
+                    </Wrapper>
+                    <Wrapper>
                         <LinkWrapper href="#about">
-                            <TypographyTitle>
-                                about
-                            </TypographyTitle>
+                            <TypographyTitle children="about" />
                         </LinkWrapper>
-                    </ListWrapper>
-                    <ListWrapper>
+                    </Wrapper>
+                    <Wrapper>
                         <LinkWrapper href="#works">
-                            <TypographyTitle>
-                                works
-                            </TypographyTitle>
+                            <TypographyTitle children="works" />
                         </LinkWrapper>
-                    </ListWrapper>
-                    <ListWrapper>
+                    </Wrapper>
+                    <Wrapper>
                         <LinkWrapper href="#contact">
-                            <TypographyTitle>
-                                contact
-                            </TypographyTitle>
+                            <TypographyTitle children="contact" />
                         </LinkWrapper>
-                    </ListWrapper>
+                    </Wrapper>
                 </List>
             </Drawer>
             <Grid 
@@ -84,30 +73,81 @@ const Nav = () => {
                     width: "100%",
                 }}
                 style={{
+                    margin: "10px",
                     position: "fixed"
                 }}
             >
-                <IconButton onClick={onToggle}>
-                    <FontAwesomeIcon 
-                        icon={faBars} 
-                        color={theme.palette.primary.main}
-                        fontSize={50}
-                    />
-                </IconButton>
-                {/* <Typography 
-                    variant="h4"
+                <IconButton 
+                    onClick={onToggle}
                     sx={{
-                        width: "100%",
-                        display: "flex",
-                        justifyContent: "center",
-                        color: theme.palette.primary.main,
+                        padding: 0,
                     }}
                 >
-                    Riku's portfolio
-                </Typography> */}
+                    <Grid
+                        sx={{
+                            borderRadius: "50%",
+                            width: {
+                                xs: "40px",
+                                sm: "60px",
+                                md: "70px",
+                                lg: "100px",
+                            },
+                            height: {
+                                xs: "40px",
+                                sm: "60px",
+                                md: "70px",
+                                lg: "100px",
+                            },
+                            fontSize: {
+                                xs: "20px",
+                                sm: "30px",
+                                md: "35px",
+                                lg: "50px",
+                            },
+                            backgroundColor: theme.palette.primary.main,
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            transition: ".3s",
+                            "&:hover": {
+                                fontSize: {
+                                    xs: "10px",
+                                    sm: "15px",
+                                    md: "17px",
+                                    lg: "25px",
+                                }
+                            }
+                        }}
+                    >
+                        <FontAwesomeIcon 
+                            icon={faBars} 
+                            color={theme.palette.secondary.main}
+                        />
+                    </Grid>
+                </IconButton>
             </Grid>
         </React.Fragment>
     )
 }
 
 export default Nav;
+
+
+
+
+// const MyCustomComponent = ({ className, children }) => (
+//     <div className={className}>
+//       {children}
+//     </div>
+//   );
+  
+//   const StyledCustomComponent = styled(MyCustomComponent)(({ className, children }) => ({
+//     color: "blue",
+//     fontSize: "16px",
+//   }))
+
+//   const App = () => (
+//     <StyledCustomComponent>
+//       This is a styled custom component.
+//     </StyledCustomComponent>
+//   );
