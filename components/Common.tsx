f90876d7ee0e4bfe68c0b8ce0d1ab01e25eb472a
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2023-12-31 00:41:15 +0900

    About.tsx: added Timeline component instead of previous one

--- components/Common.tsx
+++ components/Common.tsx
@@ -1,9 +1,20 @@
 import { styled } from "@mui/material";
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import theme from "./Theme";
+import styled2 from "@emotion/styled";
 
 export const Wrapper = styled(Box)(() => ({
     display: "flex",
     justifyContent: "center",
     alignItems: "center"
+}))
+
+export const SectionTitle = styled(Typography)(({variant="h1"}) => ({
+    // backgroundColor: "red",
+    // ".&MuiTypography-root": {
+    //     backgroundColor: "khaki",
+    // }
+    [`&.MuiTypography-${variant}`]: {
+        backgroundColor: 'khaki',
+      },
 }))
--- components/en/About.tsx
+++ components/en/About.tsx
@@ -1,9 +1,11 @@
 "use client"
 
 import { Grid, List, ListItem, Typography } from "@mui/material";
+import { Timeline, TimelineItem, TimelineSeparator, TimelineDot, TimelineConnector, TimelineContent, TimelineOppositeContent } from "@mui/lab"
 import React from "react";
 import "../Animations.css";
 import theme from "../Theme";
+import { SectionTitle } from "../Common";
 
 const About = () => {
     return (
@@ -12,9 +14,9 @@ const About = () => {
                 <Typography variant="h2">skills</Typography>
                 <Grid container item direction="row" sx={{justifyContent: "space-between"}}>
                     <Grid>
-                        <Typography variant="h3">front end</Typography>
+                        <Typography variant="h4">front end</Typography>
                         <List>
-                            <ListItem style={{padding: "0", alignItems: "flex-start"}}>JavaScript / TypeScript</ListItem>
+                            <ListItem>JavaScript / TypeScript</ListItem>
                             <ListItem>React.js / React.ts</ListItem>
                             <ListItem>Vue.js</ListItem>
                             <ListItem>jQuery</ListItem>
@@ -22,7 +24,7 @@ const About = () => {
                         </List>
                     </Grid>
                     <Grid>
-                        <Typography variant="h3">back end</Typography>
+                        <Typography variant="h4">back end</Typography>
                         <List>
                             <ListItem>Node.js</ListItem>
                             <ListItem>PHP</ListItem>
@@ -30,102 +32,94 @@ const About = () => {
                         </List>
                     </Grid>
                     <Grid>
-                        <Typography variant="h3">dev ops</Typography>
+                        <Typography variant="h4">dev ops</Typography>
                         <List>
                             <ListItem>Git / GitHub</ListItem>
                             <ListItem>Figma</ListItem>
                         </List>
                     </Grid>
                 </Grid>
-                <section className="aboutSectionSecond box">
-                    <article>
-                        <h1>about me</h1>
-                        <h3>self introduction</h3>
-                        <p>
+                <Grid container spacing={4}>
+                    <Grid item>
+                        <Typography variant="h2">about me</Typography>
+                        <Typography variant="h4">self introduction</Typography>
+                        <Typography variant="body1">
                             Hi! I'm Riku Hatano, from Japan! I have learned web development in Tamwood Careers for six months and before joinning Tamwood, I have studied this field for five months.
-                        </p>
-                    </article>
-                    <section>
-                        <h3>short history</h3>
-                        <p>Let's look back on my life a little bit!</p>
-                        <section className="eventContainer">
-                            <article className="eventCell">
-                                <p>
-                                    <span>1999.08</span>
-                                </p>
-                                <div><span></span></div>
-                                <aside>
-                                    <img src="assets/test.jpeg" alt="born" width="20vw" height="100vh"/>
-                                    {/* <img src="public/next.svg" alt="" /> */}
-                                    <figcaption>
-                                        <p>
-                                            Born in Niigata prefecture, located on north of Tokyo. I was too mischievous and oftern do stupid things to stranger though, I don't remember anything.
-                                        </p>
-                                    </figcaption>
-                                </aside>
-                            </article>
-                            <article className="eventCell">
-                                <p>
-                                    <span>2003~'15</span>
-                                </p>
-                                <div><span></span></div>
-                                <aside>
-                                    {/* <img src={Es} alt="elementalyjuniorhigh" /> */}
-                                    <figcaption>
-                                        <p>
-                                            Moved to Chiba prefecture close to Tokyo. And I have played soccer for full of 13 years.
-                                        </p>
-                                    </figcaption>
-                                </aside>
-                            </article>
-                            <article className="eventCell">
-                                <p>
-                                    <span>2015~'18</span>
-                                </p>
-                                <div><span></span></div>
-                                <aside>
-                                    {/* <img src={Hs} alt="highschool" /> */}
-                                    <figcaption>
-                                        <p>
-                                            Instead of soccer, I started to play badminton as club activity. But one year later, because of asthma, I quit to play.
-                                        </p>
-                                    </figcaption>
-                                </aside>
-                            </article>
-                            <article className="eventCell">
-                                <p>
-                                    <span>2018~'21</span>
-                                </p>
-                                <div><span></span></div>
-                                <aside>
-                                    {/* <img src={Us} alt="university" /> */}
-                                    <figcaption>
-                                        <p>
-                                            In university, I have learned Economics. Actually I enjoyed leaning though, I was interested in programming.
-                                        </p>
-                                        <p>
-                                            When COVID happened and forced to be inside of house, I touched JavaScript for the first time. 
-                                        </p>
-                                    </figcaption>
-                                </aside>
-                            </article>
-                            <article className="eventCell">
-                                <p>
-                                    <span>2022~now</span>
-                                </p>
-                                <div><span></span></div>
-                                <aside>
-                                    {/* <img src={Sa} alt="studyabroad" /> */}
-                                    <figcaption>
-                                        <p>
-                                            I have been to Dubai to study English and Vancouver to learn WebDevelopment. 
-                                        </p>
-                                    </figcaption>
-                                </aside>
-                            </article>
-                        </section>
-                    </section>
-                </section>
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant="h4">short history</Typography>
+                        <Typography variant="body1">Let's look back on my life a little bit!</Typography>
+                        <Timeline position="alternate">
+                            <TimelineItem>
+                                <TimelineOppositeContent>
+                                    1999.08
+                                </TimelineOppositeContent>
+                                <TimelineSeparator>
+                                    <TimelineDot>
+                                    </TimelineDot>
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    Born in Niigata prefecture, located on north of Tokyo. I was too mischievous and oftern do stupid things to stranger though, I don't remember anything.
+                                </TimelineContent>
+                            </TimelineItem>
+                            <TimelineItem>
+                                <TimelineOppositeContent>
+                                    2003~'15
+                                </TimelineOppositeContent>
+                                <TimelineSeparator>
+                                    <TimelineDot />
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    Moved to Chiba prefecture close to Tokyo. And I have played soccer for full of 13 years.
+                                </TimelineContent>
+                            </TimelineItem>
+                            <TimelineItem>
+                                <TimelineOppositeContent>
+                                    2015~'18
+                                </TimelineOppositeContent>
+                                <TimelineSeparator>
+                                    <TimelineDot />
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    Instead of soccer, I started to play badminton as club activity. But one year later, because of asthma, I quit to play.
+                                </TimelineContent>
+                            </TimelineItem>
+                            <TimelineItem>
+                                <TimelineOppositeContent>
+                                    2018~'21
+                                </TimelineOppositeContent>
+                                <TimelineSeparator>
+                                    <TimelineDot />
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <Typography>
+                                        In university, I have learned Economics. Actually I enjoyed leaning though, I was interested in programming.
+                                    </Typography>
+                                    <Typography>
+                                        When COVID happened and forced to be inside of house, I touched JavaScript for the first time. 
+                                    </Typography>
+                                </TimelineContent>
+                            </TimelineItem>
+                            <TimelineItem>
+                                <TimelineOppositeContent>
+                                    2022~now
+                                </TimelineOppositeContent>
+                                <TimelineSeparator>
+                                    <TimelineDot />
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    I have been to Dubai to study English and Vancouver to learn WebDevelopment. Then, I've joined internship for three months in Japan.
+                                </TimelineContent>
+                            </TimelineItem>
+                        </Timeline>
+                    </Grid>
+                </Grid>
             </Grid>
         </React.Fragment>
     )
