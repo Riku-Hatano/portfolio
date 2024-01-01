999a5b84159245b06c5286941c4872e06617585f
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2024-01-01 23:32:06 +0900

    implememted i18n

---
+++ sentences/en.ts
@@ -0,0 +1,6 @@
+const en = {
+    "welcome-title": "welcome to my portfolio site!",
+    "welcome-sentence": "Here are some works and brief self-introduction. I'm happy that you take a glance at this page!"
+}
+
+export default en;
